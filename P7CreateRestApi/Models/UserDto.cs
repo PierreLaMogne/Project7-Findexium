da271e9d2e7544abf893bf11511e06e5656cee9c
Pierre LaMogne <pierrelamogne@gmail.com>
2026-03-03 22:36:08 +0100

    Ajout du User via IdentityUser et du UserDto associé

---
+++ P7CreateRestApi/Models/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FindexiumAPI.Models
+{
+    public class UserDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
