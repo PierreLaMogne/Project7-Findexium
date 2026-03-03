9543399f16a9e0e2f036b69477cfe53ded39e244
Pierre LaMogne <pierrelamogne@gmail.com>
2026-03-03 22:32:59 +0100

    Ajout des POCO

--- P7CreateRestApi/Domain/CurvePoint.cs
+++ P7CreateRestApi/Domain/CurvePoint.cs
@@ -2,6 +2,12 @@ namespace FindexiumAPI.Domain
 {
     public class CurvePoint
     {
-        // TODO: Map columns in data table CURVEPOINT with corresponding fields
+        // DONE: Map columns in data table CURVEPOINT with corresponding fields
+        public int Id { get; set; }
+        public byte? CurveId { get; set; }
+        public DateTime? AsOfDate { get; set; }
+        public double? Term { get; set; }
+        public double? CurvePointValue { get; set; }
+        public DateTime? CreationDate { get; set; }
     }
 }
--- P7CreateRestApi/Domain/Rating.cs
+++ P7CreateRestApi/Domain/Rating.cs
@@ -2,6 +2,11 @@ namespace FindexiumAPI.Domain
 {
     public class Rating
     {
-        // TODO: Map columns in data table RATING with corresponding fields
+        // DONE: Map columns in data table RATING with corresponding fields
+        public int Id { get; set; }
+        public string MoodysRating { get; set; } = string.Empty;
+        public string SandPRating { get; set; } = string.Empty;
+        public string FitchRating { get; set; } = string.Empty;
+        public byte? OrderNumber { get; set; }
     }
 }
