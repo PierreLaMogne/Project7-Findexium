55e2f94fc3d81f6042b86ba49abdac816c235e7e
Pierre LaMogne <pierrelamogne@gmail.com>
2026-03-08 19:18:20 +0100

    Implémentation de Result<T> dans le UserRepository

--- P7CreateRestApi/Common/Result.cs
+++ P7CreateRestApi/Common/Result.cs
@@ -2,18 +2,18 @@
 {
     public class Result<T>
     {
-        public bool Success { get; set; }
+        public bool IsSuccess { get; set; }
         public T Data { get; set; } = default!;
         public string ErrorMessage { get; set; } = string.Empty;
 
 
         public static Result<T> Ok(T data)
         {
-            return new Result<T> { Success = true, Data = data };
+            return new Result<T> { IsSuccess = true, Data = data };
         }
         public static Result<T> Fail(string errorMessage)
         {
-            return new Result<T> { Success = false, ErrorMessage = errorMessage };
+            return new Result<T> { IsSuccess = false, ErrorMessage = errorMessage };
         }
     }
 }
