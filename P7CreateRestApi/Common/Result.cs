7ddcfb0ceaa27ee0d4dd7d3e6fb827136c3ed562
Pierre LaMogne <pierrelamogne@gmail.com>
2026-03-09 15:40:30 +0100

    Ajout du Result.Code pour indiquer le code HTTP à renvoyer

--- P7CreateRestApi/Common/Result.cs
+++ P7CreateRestApi/Common/Result.cs
@@ -5,15 +5,16 @@
         public bool IsSuccess { get; set; }
         public T Data { get; set; } = default!;
         public string ErrorMessage { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
 
 
         public static Result<T> Ok(T data)
         {
             return new Result<T> { IsSuccess = true, Data = data };
         }
-        public static Result<T> Fail(string errorMessage)
+        public static Result<T> Fail(string errorMessage, string code)
         {
-            return new Result<T> { IsSuccess = false, ErrorMessage = errorMessage };
+            return new Result<T> { IsSuccess = false, ErrorMessage = errorMessage, Code = code };
         }
     }
 }
