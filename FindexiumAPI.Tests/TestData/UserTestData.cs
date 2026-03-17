e6a5c21f72b797940bb2e573590290214b7cd163
Pierre LaMogne <pierrelamogne@gmail.com>
2026-03-17 17:52:12 +0100

    Implémentation du UserRepositoryTests + Modification du UserRepository

---
+++ FindexiumAPI.Tests/TestData/UserTestData.cs
@@ -0,0 +1,196 @@
+﻿using Castle.Core.Configuration;
+using FindexiumAPI.Domain;
+using FindexiumAPI.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace FindexiumAPI.Tests.TestData
+{
+    public class UserTestData
+    {
+        public static IEnumerable<object[]> GetUsersScenarios()
+        {
+            yield return new object[]
+            {
+                new List<User>()
+            };
+
+            yield return new object[]
+            {
+                new List<User>
+                {
+                    new User { Id = 1.ToString(), UserName = "abc", FullName = "Abc", Role = "Admin"}
+                }
+            };
+
+            yield return new object[]
+            {
+                new List<User>()
+                {
+                    new User { Id = 2.ToString(), UserName = "def", FullName = "Def", Role = "Admin"},
+                    new User { Id = 3.ToString(), UserName = "ghi", FullName = "Ghi", Role = "User"}
+                }
+            };
+        }
+
+        public static IEnumerable<object[]> GetCreateUsersScenarios()
+        {
+            yield return new object[]
+            {
+                new CreateUserDto()
+                {
+                    UserName = "abc",
+                    FullName = "Abc",
+                    Role = "Admin",
+                    Password = "Bonjour123!",
+                    ConfirmPassword = "Bonjour123!"
+                }
+            };
+
+            yield return new object[]
+            {
+                new CreateUserDto()
+                {
+                    UserName = "def",
+                    FullName = "Def",
+                    Role = "Admin",
+                    Password = "Bonjour123!",
+                    ConfirmPassword = "Bonjour123!"
+                }
+            };
+
+            yield return new object[]
+            {
+                new CreateUserDto()
+                {
+                    UserName = "ghi",
+                    FullName = "Ghi",
+                    Role = "User",
+                    Password = "Bonjour123!",
+                    ConfirmPassword = "Bonjour123!"
+                }
+            };
+
+            yield return new object[]
+            {
+                new CreateUserDto()
+                {
+                    UserName = "jkl",
+                    FullName = "Jkl",
+                    Role = "NotARole",
+                    Password = "Bonjour123!",
+                    ConfirmPassword = "Bonjour123!"
+                }
+            };
+
+            yield return new object[]
+            {
+                new CreateUserDto()
+                {
+                    UserName = "mno",
+                    FullName = "Mno",
+                    Password = "Bonjour123!",
+                    ConfirmPassword = "Bonjour123!"
+                }
+            };
+
+            yield return new object[]
+            {
+                new CreateUserDto()
+                {
+                    UserName = "pqr",
+                    FullName = "Pqr",
+                    Role = "Admin",
+                    Password = "Bonjour123!",
+                    ConfirmPassword = "Aurevoir456?"
+                }
+            };
+        }
+
+
+        public static IEnumerable<object[]> GetUsersToUpdateScenarios()
+        {
+            yield return new object[]
+            {
+                new List<User>()
+                {
+                    new User { Id = 1.ToString(), UserName = "abc", FullName = "Abc", Role = "Admin"},
+                    new User { Id = 2.ToString(), UserName = "def", FullName = "Def", Role = "Admin"},
+                    new User { Id = 3.ToString(), UserName = "ghi", FullName = "Ghi", Role = "User"},
+                    new User { Id = 4.ToString(), UserName = "alreadyExists", FullName = "Already Exists", Role = "User"}
+                }
+            };
+        }
+
+        public static IEnumerable<object[]> GetUserUpdatesScenarios()
+        {
+            yield return new object[]
+            {
+                new UserDto { Id = 1.ToString(), UserName = "abc_updated", FullName = "Abc Updated", Role = "User"}
+            };
+            yield return new object[]
+            {
+                new UserDto { Id = 2.ToString(), UserName = "def_updated", FullName = "Def Updated", Role = ""}
+            };
+            yield return new object[]
+            {
+                new UserDto { Id = 3.ToString(), UserName = "ghi_updated", FullName = "Ghi Updated", Role = "Admin"}
+            };
+            yield return new object[]
+            {
+                new UserDto { Id = 1.ToString(), UserName = "alreadyExists", FullName = "Already Exists", Role = "User"}
+            };
+            yield return new object[]
+            {
+                new UserDto { Id = 5.ToString(), UserName = "wrongId", FullName = "Wrong Id", Role = "Admin"}
+            };
+        }
+
+        public static IEnumerable<object[]> GetUpdateUserScenariosWithData()
+        {
+            yield return new object[]
+            {
+                new List<User>()
+                {
+                    new User { Id = 1.ToString(), UserName = "abc", FullName = "Abc", Role = "Admin"},
+                    new User { Id = 2.ToString(), UserName = "def", FullName = "Def", Role = "Admin"},
+                    new User { Id = 3.ToString(), UserName = "ghi", FullName = "Ghi", Role = "User"},
+                    new User { Id = 4.ToString(), UserName = "alreadyExists", FullName = "Already Exists", Role = "User"}
+                },
+                new UserDto { Id = 1.ToString(), UserName = "abc_updated", FullName = "Abc Updated", Role = "User"}
+            };
+            yield return new object[]
+            {
+                new List<User>()
+                {
+                    new User { Id = 1.ToString(), UserName = "abc", FullName = "Abc", Role = "Admin"},
+                    new User { Id = 2.ToString(), UserName = "def", FullName = "Def", Role = "Admin"},
+                    new User { Id = 3.ToString(), UserName = "ghi", FullName = "Ghi", Role = "User"},
+                    new User { Id = 4.ToString(), UserName = "alreadyExists", FullName = "Already Exists", Role = "User"}
+                },
+                new UserDto { Id = 2.ToString(), UserName = "def_updated", FullName = "Def Updated", Role = ""}
+            };
+            yield return new object[]
+            {
+                new List<User>()
+                {
+                    new User { Id = 1.ToString(), UserName = "abc", FullName = "Abc", Role = "Admin"},
+                    new User { Id = 2.ToString(), UserName = "def", FullName = "Def", Role = "Admin"},
+                    new User { Id = 3.ToString(), UserName = "ghi", FullName = "Ghi", Role = "User"},
+                    new User { Id = 4.ToString(), UserName = "alreadyExists", FullName = "Already Exists", Role = "User"}
+                },
+                new UserDto { Id = 3.ToString(), UserName = "ghi_updated", FullName = "Ghi Updated", Role = "Admin"}
+            };
+            yield return new object[]
+            {
+                new List<User>()
+                {
+                    new User { Id = 1.ToString(), UserName = "abc", FullName = "Abc", Role = "Admin"},
+                    new User { Id = 2.ToString(), UserName = "def", FullName = "Def", Role = "Admin"},
+                    new User { Id = 3.ToString(), UserName = "ghi", FullName = "Ghi", Role = "User"},
+                    new User { Id = 4.ToString(), UserName = "alreadyExists", FullName = "Already Exists", Role = "User"}
+                },
+                new UserDto { Id = 1.ToString(), UserName = "alreadyExists", FullName = "Already Exists", Role = "User"}
+            };
+        }
+    }
+}
--- FindexiumAPI.Tests/obj/Debug/net6.0/FindexiumAPI.Tests.AssemblyInfo.cs
+++ FindexiumAPI.Tests/obj/Debug/net6.0/FindexiumAPI.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FindexiumAPI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8530a58e74c74850f6d19943bc4e3fdfd5171e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c4885c0aec3e65233e37b53b0ac71715a3c7503")]
 [assembly: System.Reflection.AssemblyProductAttribute("FindexiumAPI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FindexiumAPI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
